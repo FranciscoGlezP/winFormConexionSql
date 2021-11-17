af05da64bd8a3d650279b2d8bf83078cc91100e1
franPintor <fco.jav.glez.p@gmail.com>
2021-11-16 20:58:01 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace winFormConexionSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtBD = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPw = new System.Windows.Forms.TextBox();
+            this.btnPruebaConexion = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Base de Datos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 343);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contraseña";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(201, 50);
+            this.txtHost.Multiline = true;
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(349, 79);
+            this.txtHost.TabIndex = 4;
+            // 
+            // txtBD
+            // 
+            this.txtBD.Location = new System.Drawing.Point(201, 147);
+            this.txtBD.Name = "txtBD";
+            this.txtBD.Size = new System.Drawing.Size(100, 22);
+            this.txtBD.TabIndex = 5;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(201, 257);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 22);
+            this.txtUser.TabIndex = 6;
+            // 
+            // txtPw
+            // 
+            this.txtPw.Location = new System.Drawing.Point(201, 356);
+            this.txtPw.Name = "txtPw";
+            this.txtPw.Size = new System.Drawing.Size(100, 22);
+            this.txtPw.TabIndex = 7;
+            // 
+            // btnPruebaConexion
+            // 
+            this.btnPruebaConexion.Location = new System.Drawing.Point(377, 343);
+            this.btnPruebaConexion.Name = "btnPruebaConexion";
+            this.btnPruebaConexion.Size = new System.Drawing.Size(173, 48);
+            this.btnPruebaConexion.TabIndex = 8;
+            this.btnPruebaConexion.Text = "Prueba la conexion";
+            this.btnPruebaConexion.UseVisualStyleBackColor = true;
+            this.btnPruebaConexion.Click += new System.EventHandler(this.btnPruebaConexion_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPruebaConexion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPw);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUser);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBD);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtHost);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(678, 397);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese a su Conexion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 439);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Conexio a MySql";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtBD;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPw;
+        private System.Windows.Forms.Button btnPruebaConexion;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winFormConexionSql
+{
+    public partial class Form1 : Form
+    {
+
+        #region Propiedades
+        private MySql.Data.MySqlClient.MySqlConnection conexion;
+        #endregion Propiedades
+
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion Constructor
+
+        #region Eventos
+        private void btnPruebaConexion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion = new MySql.Data.MySqlClient.MySqlConnection();
+                conexion.ConnectionString = string.Format("server={0};uid={1};pwd={2};database={3}",
+                               txtHost.Text,txtUser.Text,txtPw.Text,txtBD.Text);
+                conexion.Open();
+                if (conexion.State == System.Data.ConnectionState.Open)
+                {
+                    MessageBox.Show("¡Conexion Exitosa!");
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show("Error de Conexion: " + ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+
+        }
+        #endregion Eventos
+    }
+}
